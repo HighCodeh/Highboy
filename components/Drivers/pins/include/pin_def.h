3e0417cd69106b43fedd71c30ac92db3660a5f41
luis_thiago <95657866+lthiagovs@users.noreply.github.com>
2025-12-01 20:51:12 -0300

    refactor(pins): remove redundant button definitions

--- components/Drivers/pins/include/pin_def.h
+++ components/Drivers/pins/include/pin_def.h
@@ -31,11 +31,7 @@
 #define BTN_UP     15
 #define BTN_DOWN   6
 #define BTN_OK     4
-#define BTN_BACK   7
-#define BTN_LEFT 5  
 #define BTN_RIGHT 16
-#define SD_CARD_PIN_MISO    GPIO_NUM_13
-#define SD_CARD_PIN_CS      GPIO_NUM_14
 
 // led
 #define LED_GPIO     45
